fc0d8f96d190e442010dcae86d444d8d677e83a8
osbert <kamukamaosbert2023@gmail.com>
2026-04-25 11:18:00 +0300

    fix google

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+from .models import UserProfile
 
-# Register your models here.
+
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    extra = 0
+    fields = ("role", "phone_number", "department", "is_available")
+
+
+class CustomUserAdmin(UserAdmin):
+    inlines = (UserProfileInline,)
+    list_display = ("username", "email", "is_staff", "is_superuser")
+    search_fields = ("username", "email")
+
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
